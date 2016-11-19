dc6b1036e536e52d4a0ddd437725ed0cc7fb1c99
WDavidO <WDavidO@users.noreply.github.com>
2016-11-18 21:04:47 -0500

    Major update. Decryption code is written. Still lacking serialization code (though it is pretty much there in buffer1024...) and clean up into a nice useable library with C interface for those that desire one.

--- Rabin1024.h
+++ Rabin1024.h
@@ -2,30 +2,35 @@
 #include <openssl/bn.h>
 #include <stdint.h>
 
-extern "C" void extended_GCD(BIGNUM *x, BIGNUM *y,BIGNUM *gcd,BIGNUM *a,BIGNUM *b);//ax+by=gcd
+extern "C" void extended_GCD(const BIGNUM *x, const BIGNUM *y, BIGNUM *gcd,BIGNUM *a,BIGNUM *b, BN_CTX *ctx);//ax+by=gcd
 
-struct buffer1024{
+struct buffer1024 {
+public:
     uint8_t values[128];
+    void toBN(BIGNUM * dest) const;
+    void fromBN(const BIGNUM * src);
 };
 
-class Rabin1024{
+class Rabin1024 {
 private:
-    BIGNUM* p,*q,*n;
-    BN_CTX *ctx;
+    BIGNUM *m_p,*m_q,*m_n,*m_a,*m_b;
+    BN_CTX *m_ctx;
     void commonConstruct();
     void generatePrimes();
     void seedRandom();
     void testAndFreeBN(BIGNUM * val);
     void genPrime(BIGNUM * Prime);
+    void calculateAB();
 public:
-    Rabin1024(BIGNUM* n);//create with only decryption capabilities
-    Rabin1024(BIGNUM* p, BIGNUM* q,BIGNUM* n=NULL);//create with both if n is NULL we'll calculate it
+    Rabin1024(const BIGNUM* n);//create with only decryption capabilities
+    Rabin1024(const BIGNUM* p, const BIGNUM* q, const BIGNUM* n=NULL);//create with both if n is NULL we'll calculate it
+    Rabin1024(const BIGNUM* p, const BIGNUM* q, const BIGNUM *A, const BIGNUM *B, const BIGNUM* n=NULL);//if a and b are cached for speed
     Rabin1024();//no key data so we'll generate them
     ~Rabin1024();
     //returns 1 for success negative error code otherwise
-    int8_t decrypt(buffer1024 * cipherText, buffer1024 * arrayOf4Solutions);
+    int8_t decrypt(const buffer1024 &cipherText, buffer1024 (&arrayOf4Solutions)[4]);
     //returns 1 for success negative error code otherwise
-    int8_t encrypt(buffer1024 * plainText, buffer1024* cipherText);
+    int8_t encrypt(const buffer1024 &plainText, buffer1024 &cipherText);
     void printDecData();
 };
 
